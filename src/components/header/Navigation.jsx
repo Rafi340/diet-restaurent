50aa4ef46b1b78b088df27231d695a09febdf052
Rafi340 <rafisamnan340@gmail.com>
2023-01-09 00:27:24 +0600

    edit: navigation edit

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -3,10 +3,16 @@ import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import React from 'react';
 
 const { Header, Content, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-  }));
+const items1 = [
+  {
+    key: 2,
+    label: `DIET Restaurent`
+  },
+  {
+  key: 1,
+  label: `Nav 1`,
+  }
+];
   const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
     const key = String(index + 1);
     return {
@@ -34,23 +40,7 @@ const Navigation = () => {
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
     </Header>
     <Layout>
-      <Sider
-        width={200}
-        style={{
-          background: colorBgContainer,
-        }}
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{
-            height: '100%',
-            borderRight: 0,
-          }}
-          items={items2}
-        />
-      </Sider>
+  
       <Layout
         style={{
           padding: '0 24px 24px',
